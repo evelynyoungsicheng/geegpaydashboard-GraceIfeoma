20dcdfea20cb6fedc191c663184870b0dfdc5535
Ghreizm <48857032+evelynyoungsicheng@users.noreply.github.com>
2024-01-27 01:12:06 +0100

    Delete Geegpay Dashboard- Ifeoma Ifechukwu directory

--- Geegpay Dashboard- Ifeoma Ifechukwu/toggle.js
+++
@@ -1,9 +0,0 @@
-
-const body = document.querySelector("body")
-const switchMode = document.querySelector('.switchmode');
-
-function myFunction(){
-      switchMode.addEventListener("click",() =>{
-          body.classList.toggle("dark");
-      })
-    }
